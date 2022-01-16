855999b2b63302fe9073d9b55515f7a05a56e858
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-01-16 12:43:18 +0100

    only switch UID when differs from current

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
 UID=`stat -c "%u" /workspace`
-if [ $UID -ne 0 ]; then
+if [ $UID -ne 0 -a $UID -ne `uid -u $BUILD_USER` ]; then
 	usermod -u $UID $BUILD_USER
 	gosu $BUILD_USER "$@"
 else
