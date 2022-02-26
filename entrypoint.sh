df0e349830bda21b1b317623ae7942b50e829af3
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-02-26 15:05:37 +0100

    use /bin/bash as default command

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ umask 0022
 
 UID=`stat -c "%u" /workspace`
 [ $UID -ne 0 -a $UID -ne `id -u $BUILD_USER` ] && usermod -u $UID $BUILD_USER
-gosu $BUILD_USER "$@"
+
+[ "$#" -gt 0 ] && ARGS="$@" || ARGS="/bin/bash"
+gosu $BUILD_USER "$ARGS"
 
