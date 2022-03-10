0763ed29c63b3f0e5aad1445b9ed43ee714580c9
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-03-10 08:16:36 +0100

    fix for args/no args calls to gosu

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,5 @@ umask 0022
 WS_OWNER=`stat -c "%u" /workspace`
 [ $WS_OWNER -ne 0 -a $WS_OWNER -ne `id -u $BUILD_USER` ] && usermod -u $WS_OWNER $BUILD_USER
 
-[ "$#" -gt 0 ] && ARGS=$@ || ARGS=$SHELL
-exec gosu $BUILD_USER $ARGS
+[ "$#" -gt 0 ] && gosu "$BUILD_USER" "$@" || gosu "$BUILD_USER" "$SHELL"
 
