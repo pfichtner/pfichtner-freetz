002e398ff2c32ef3d0abbf34269932ac0f52de43
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-06-10 09:13:55 +0200

    do not sudo if not root

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,5 +7,10 @@ umask 0022
 WS_OWNER=`stat -c "%u" /workspace`
 [ $WS_OWNER -ne 0 -a $WS_OWNER -ne `id -u $BUILD_USER` ] && usermod -u $WS_OWNER $BUILD_USER
 
-[ "$#" -gt 0 ] && gosu "$BUILD_USER" "$@" || gosu "$BUILD_USER" "$SHELL"
+DEFAULT_SHELL=`getent passwd $BUILD_USER | cut -f 7 -d':'`
+if [ `id -u` -eq 0 ]; then
+	[ "$#" -gt 0 ] && gosu "$BUILD_USER" "$@" || gosu "$BUILD_USER" "$DEFAULT_SHELL"
+else
+	[ "$#" -gt 0 ] && "$@" || "$DEFAULT_SHELL"
+fi
 
