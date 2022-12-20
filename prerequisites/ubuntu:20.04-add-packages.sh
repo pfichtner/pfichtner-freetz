058848005b1c834d1dacf8b3b443ba70896b17ae
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-20 12:47:43 +0100

    changed from extracting packages to extracting commands

--- prerequisites/ubuntu:20.04-add-packages.sh
+++ prerequisites/ubuntu:20.04-add-packages.sh
@@ -1,18 +1,18 @@
 # for convenience inside the container
-command-not-found vim-gtk3 locales
+apt-get -y install command-not-found vim-gtk3 locales
 
 # not for freetz but this docker image to switch to unprivileged user in entrypoint
-gosu
+apt-get -y install gosu
 
 ######################################################################################################
 ######################################################################################################
 
 # missing in Freeet-NG's .prerequisites -> .prerequisites:program|uudecode|FREETZ_PACKAGE_ASTERISK_GUI
-sharutils
+apt-get -y install sharutils
 
 # not necessary for building but by tools/freetz_patch (lsdiff filterdiff)
-patchutils
+apt-get -y install patchutils
 
 # not necessary for building but uploading via tools/push_firmware
-iproute2 ncftp iputils-ping net-tools
+apt-get -y install iproute2 ncftp iputils-ping net-tools
 
