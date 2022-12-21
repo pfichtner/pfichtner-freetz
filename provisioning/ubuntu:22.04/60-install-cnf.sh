f1416dec8aef83ccadc0d554cd46ac023616e646
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-21 12:12:12 +0100

    redirect fix

--- provisioning/ubuntu:22.04/60-install-cnf.sh
+++ provisioning/ubuntu:22.04/60-install-cnf.sh
@@ -5,7 +5,7 @@ apt-get -y update
 
 # we need the patch binary so get sure it's really there 
 # (we could uninstall it afterwards but leave it there, it doesn't harm)
-command -v patch 2>/dev/null 2>&1 || apt-get -y install patch
+command -v patch >/dev/null 2>&1 || apt-get -y install patch
 
 patch -p0 <$PROVISION_DIR/patch-cnf-autoinstall.patch
 rm $PROVISION_DIR/patch-cnf-autoinstall.patch
