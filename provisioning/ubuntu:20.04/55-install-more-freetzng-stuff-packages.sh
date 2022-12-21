b84cc65e2805e87ff43d41a069341d2ec9ddd98e
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-21 16:40:36 +0100

    describing why packages are installed for

--- provisioning/ubuntu:20.04/55-install-more-freetzng-stuff-packages.sh
+++ provisioning/ubuntu:20.04/55-install-more-freetzng-stuff-packages.sh
@@ -2,5 +2,9 @@
 apt-get -y install patchutils
 
 # not necessary for building but uploading via tools/push_firmware
+# iproute2: /bin/ip /sbin/bridge /sbin/ip (not needed but useful)
+# ncftp: /usr/bin/ncftpput
+# iputils-ping: /bin/ping
+# net-tools: /sbin/ifconfig
 apt-get -y install iproute2 ncftp iputils-ping net-tools
 
