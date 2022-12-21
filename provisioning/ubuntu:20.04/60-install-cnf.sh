45797ee5700f9c98a13acfeaa1d25d495f434481
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-21 10:08:51 +0100

    splitted into smaller junks

---
+++ provisioning/ubuntu:20.04/60-install-cnf.sh
@@ -0,0 +1,12 @@
+apt-get -y install command-not-found
+
+# need to run again after installing c-n-f
+apt-get -y update
+
+# we need the patch binary so get sure it's really there 
+# (we could uninstall it afterwards but leave it there, it doesn't harm)
+command -v patch 2>/dev/null || apt-get -y install patch
+
+patch -p0 <$PROVISION_DIR/patch-cnf-autoinstall.patch
+rm $PROVISION_DIR/patch-cnf-autoinstall.patch
+
