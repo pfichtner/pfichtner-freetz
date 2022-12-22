22b714d3faee3f836e157dfb5edb63bced2014c5
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-22 12:48:54 +0100

    parameterized fedora version

--- provisioning/fedora:36/55-install-dockerimage-packages.sh
+++ provisioning/fedora:36/55-install-dockerimage-packages.sh
@@ -1,7 +1,8 @@
 # add repo
 dnf -y install dnf-utils
-rpm --import "http://downloadcontent.opensuse.org/repositories/home:/alvistack/Fedora_36/repodata/repomd.xml.key"
-yum-config-manager --add-repo "http://downloadcontent.opensuse.org/repositories/home:/alvistack/Fedora_36"
+. /etc/os-release 
+rpm --import "http://downloadcontent.opensuse.org/repositories/home:/alvistack/Fedora_$VERSION_ID/repodata/repomd.xml.key"
+yum-config-manager --add-repo "http://downloadcontent.opensuse.org/repositories/home:/alvistack/Fedora_$VERSION_ID"
 
 # our docker entrypoint relies on gosu to switch to unprivileged user
 dnf -y install gosu
