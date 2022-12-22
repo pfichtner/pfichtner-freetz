9115227055d4cb163b6328ec4abbb70758498573
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-22 16:44:09 +0100

    removing ecj, installing openjdk-8

--- provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
@@ -1,7 +1,5 @@
 # program     rpcgen                 FREETZ_PACKAGE_NFS_UTILS FREETZ_PACKAGE_AUTOFS
 # program     javac                  FREETZ_PACKAGE_CLASSPATH
-dnf -y install rpcgen python-unversioned-command
-
-ln -sf /usr/bin/ecj /etc/alternatives/javac
-ln -sf /etc/alternatives/javac /usr/bin/javac
+dnf -y remove ecj
+dnf -y install rpcgen python-unversioned-command java-1.8.0-openjdk-devel
 
--- provisioning/ubuntu:20.04/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/ubuntu:20.04/51-fix-freetz-ng-prerequisites.sh
@@ -1,3 +1,5 @@
 # .prerequisites: program     xml2-config            FREETZ_PACKAGE_GNTPSEND FREETZ_PACKAGE_ASTERISK
-apt-get -y install libxml2-dev
+# program     javac                  FREETZ_PACKAGE_CLASSPATH
+apt-get -y purge ecj
+apt-get -y install libxml2-dev openjdk-8-jdk-headless
 
