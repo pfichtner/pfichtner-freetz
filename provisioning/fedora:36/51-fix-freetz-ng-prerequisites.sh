1ed8f18195ab02b49ca8f1442b84984b64f792cd
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-22 12:49:12 +0100

    java-devel added

--- provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
@@ -1,2 +1,4 @@
-dnf -y install rpcgen python-unversioned-command
+# program     rpcgen                 FREETZ_PACKAGE_NFS_UTILS FREETZ_PACKAGE_AUTOFS
+# program     javac                  FREETZ_PACKAGE_CLASSPATH
+dnf -y install rpcgen python-unversioned-command java-devel
 
