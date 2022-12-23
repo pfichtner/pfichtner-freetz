b229002b3344a17aa818fdabf19f08f33b26e5ef
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-23 11:02:09 +0100

    install java-wrappers

--- provisioning/ubuntu:22.04/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/ubuntu:22.04/51-fix-freetz-ng-prerequisites.sh
@@ -1,5 +1,4 @@
 # .prerequisites: program     xml2-config            FREETZ_PACKAGE_GNTPSEND FREETZ_PACKAGE_ASTERISK
 # program     javac                  FREETZ_PACKAGE_CLASSPATH
-apt-get -y purge ecj
-apt-get -y install libxml2-dev openjdk-8-jdk-headless
+apt-get -y install libxml2-dev java-wrappers
 
