fd6ddf5e4684c63f21120ae0af11b3a6f1cd065c
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-30 21:49:40 +0100

    supporting debian/ubuntu/fedora

--- scripts/dependency-diff.sh
+++ scripts/dependency-diff.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
 deps() {
-	FILE="$1/50-freetz-ng-prerequisites.sh"
-	sed -n -E 's/^sudo apt-get -y install |^sudo apt -y install //p' $FILE | tr ' ' '\n' | grep . | sort | uniq
+	FILTER="$1"
+	FILE="$2/50-freetz-ng-prerequisites.sh"
+	sed -n -E 's/'"$FILTER"'//p' $FILE | tr ' ' '\n' | grep . | sort | uniq
+}
+
+ubuntuDeps() {
+	debs "$1" 
 }
 
 calcDiff() {
-	DIFF=$(diff -Naur <(deps "$2") <(deps "$1") | grep -v ' ' | sort | tr '\n' ' ')
-	echo "| $2 | $1 | $DIFF |"
+	DIFF=$(diff -Naur <(deps "$1" "$3") <(deps "$1" "$2") | grep -v ' ' | sort | tr '\n' ' ')
+	echo "| $3 | $2 | $DIFF |"
 }
 
 
 echo "| From | To | Changes |"
 echo "| - | - | - |"
+PROVISIONING_DIR="provisioning"
+distroDiff() {
+	DIRS=$(cd "$PROVISIONING_DIR" && ls -d $1)
+	PREV=""
+	echo "$DIRS" | tac | while read DISTRO; do
+		[ -n "$PREV" ] && calcDiff "$2" "$PROVISIONING_DIR/$PREV" "$PROVISIONING_DIR/$DISTRO"
+		PREV="$DISTRO"
+	done
+}
 
-cd provisioning
-PREV=""
-ls -d ubuntu* | tac | while read DISTRO; do
-	[ -n "$PREV" ] && calcDiff "$PREV" "$DISTRO"
-	PREV="$DISTRO"
-done
+distroDiff 'ubuntu*' '^sudo apt-get -y install |^sudo apt -y install '
+distroDiff 'debian*' '^sudo apt-get -y install |^sudo apt -y install '
+distroDiff 'fedora*' '^sudo dnf -y install'
 
