82101c0e6ea65b9c200fb31f7fdcc7c241e0daee
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-30 16:26:14 +0100

    write dependency diff file

---
+++ scripts/dependency-diff.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+deps() {
+	FILE="provisioning/$1/50-freetz-ng-prerequisites.sh"
+	sed -n -E 's/^sudo apt-get -y install |^sudo apt -y install //p' $FILE | tr ' ' '\n' | grep . | sort | uniq
+}
+
+calcDiff() {
+	DIFF=$(diff -Naur <(deps "$2") <(deps "$1") | grep -v ' ' | sort | tr '\n' ' ')
+	echo diff "$1 to $2: $DIFF"
+}
+
+calcDiff "ubuntu:22.04" "ubuntu:20.04"
+calcDiff "ubuntu:20.04" "ubuntu:18.04"
+calcDiff "ubuntu:18.04" "ubuntu:16.04"
+calcDiff "ubuntu:16.04" "ubuntu:14.04"
+
