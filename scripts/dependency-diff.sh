2d3fbf065a65e2708ceec96f91572f973e8af27c
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-30 21:08:26 +0100

    listing all ubuntu versions

--- scripts/dependency-diff.sh
+++ scripts/dependency-diff.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 deps() {
-	FILE="provisioning/$1/50-freetz-ng-prerequisites.sh"
+	FILE="$1/50-freetz-ng-prerequisites.sh"
 	sed -n -E 's/^sudo apt-get -y install |^sudo apt -y install //p' $FILE | tr ' ' '\n' | grep . | sort | uniq
 }
 
 calcDiff() {
 	DIFF=$(diff -Naur <(deps "$2") <(deps "$1") | grep -v ' ' | sort | tr '\n' ' ')
-	echo "+$1 -$2: $DIFF"
+	echo "$2 -> $1: $DIFF"
 }
 
-calcDiff "ubuntu:22.04" "ubuntu:20.04"
-calcDiff "ubuntu:20.04" "ubuntu:18.04"
-calcDiff "ubuntu:18.04" "ubuntu:16.04"
-calcDiff "ubuntu:16.04" "ubuntu:14.04"
+
+PREV=""
+
+cd provisioning
+ls -d ubuntu* | tac | while read DISTRO; do
+	[ -n "$PREV" ] && calcDiff "$PREV" "$DISTRO"
+	PREV="$DISTRO"
+done
 
