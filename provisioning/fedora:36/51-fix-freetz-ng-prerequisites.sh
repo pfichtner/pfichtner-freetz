c0ea7ab6c6b6db1a091bd3f223ab41ccf0a24165
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-30 11:27:25 +0100

    creating links via update-alternatives

--- provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
@@ -1,7 +1,5 @@
 # program     rpcgen                 FREETZ_PACKAGE_NFS_UTILS FREETZ_PACKAGE_AUTOFS
 # program     javac                  FREETZ_PACKAGE_CLASSPATH
 dnf -y install rpcgen python-unversioned-command javapackages-tools
-ln -sf /usr/bin/ecj /etc/alternatives/javac
-ln -sf /etc/alternatives/javac /usr/bin/javac
-
+update-alternatives --install /usr/bin/javac javac /usr/bin/ecj 20
 
