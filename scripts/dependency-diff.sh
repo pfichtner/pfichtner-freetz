26f6ec209459360dc08e7bdb70ac066ebfa88dc5
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-31 11:15:20 +0100

    use json files instead of parsing

--- scripts/dependency-diff.sh
+++ scripts/dependency-diff.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 deps() {
-	FILTER="$1"
-	FILE="$PROVISIONING_DIR/$2/50-freetz-ng-prerequisites.sh"
-	sed -n -E 's/'"$FILTER"'//p' $FILE | tr ' ' '\n' | grep . | sort | uniq
+	FILE="$PROVISIONING_DIR/$1/dependencies.json"
+	cat "$FILE" | jq -r '[.packages] | flatten[]' | grep . | sort | uniq
 }
 
 calcDiff() {
-	DIFF=$(diff -Naur <(deps "$1" "$3") <(deps "$1" "$2") | grep -v ' ' | sort | tr '\n' ' ')
-	echo "| $3 | $2 | $DIFF |"
+	DIFF=$(diff -Naur <(deps "$2") <(deps "$1") | grep -v ' ' | sort | tr '\n' ' ')
+	echo "| $2 | $1 | $DIFF |"
 }
 
 
@@ -20,12 +19,12 @@ distroDiff() {
 	DIRS=$(cd "$PROVISIONING_DIR" && ls -d $1)
 	PREV=""
 	echo "$DIRS" | tac | while read DISTRO; do
-		[ -n "$PREV" ] && calcDiff "$2" "$PREV" "$DISTRO"
+		[ -n "$PREV" ] && calcDiff "$PREV" "$DISTRO"
 		PREV="$DISTRO"
 	done
 }
 
-distroDiff 'ubuntu*' '^sudo apt-get -y install |^sudo apt -y install '
-distroDiff 'debian*' '^sudo apt-get -y install |^sudo apt -y install '
-distroDiff 'fedora*' '^sudo dnf -y install'
+distroDiff 'ubuntu*'
+distroDiff 'debian*'
+distroDiff 'fedora*'
 
