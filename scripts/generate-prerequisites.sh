721df5bdefa781c451437a536045107b46ada810
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-31 10:57:35 +0100

    generating JSON files as well

--- scripts/generate-prerequisites.sh
+++ scripts/generate-prerequisites.sh
@@ -4,6 +4,42 @@ HTTP_SOURCE=$1
 CACHE=$2
 TARGET=$3
 
+deps() {
+	CONTENT="$1"
+	FILTER="$2"
+	echo "$CONTENT" | sed -n -E 's/'"$FILTER"'//p' | tr ' ' '\n' | grep . | sort | uniq
+}
+
+content() {
+	SOURCE_FILE="$1"
+	DISTRO_ENTRY="$2"
+	cat "$SOURCE_FILE" | \
+	# find relevant section (ignore all lines before)
+	sed -n "/$DISTRO_ENTRY/,\$p" | \
+	# find content between "```"
+	sed -n '/```/{:loop n; /```/q; p; b loop}'
+}
+
+writeDepsJsonFile() {
+	SOURCE_FILE="$1"
+	TARGET_FILE="$2"
+	OVERWRITE="$3"
+	DISTRO_ENTRY="$4"
+	PATTERN="$5"
+
+	[ -e "$TARGET_FILE" ] && [ "$OVERWRITE" = "false" ] && return
+
+	[ -d `dirname "$TARGET_FILE"` ] || mkdir -p `dirname "$TARGET_FILE"`
+	
+ 	CONTENT=$(content "$SOURCE_FILE" "$DISTRO_ENTRY") 
+	PACKAGES=$(jq -R -s -c 'split("\n") | map(select(length > 0))' <(deps "$CONTENT" "$PATTERN"))
+
+	TEMPLATE='{ "packages": "%s" }'
+	DATA=""
+	printf -v DATA "$TEMPLATE" "$PACKAGES"
+	echo "$DATA" >"$TARGET_FILE"
+}
+
 writePackageFile() {
 	SOURCE_FILE="$1"
 	TARGET_FILE="$2"
@@ -15,12 +51,22 @@ writePackageFile() {
 	[ -d `dirname "$TARGET_FILE"` ] || mkdir -p `dirname "$TARGET_FILE"`
 
 	PREFIX='sudo() { eval ${*@Q}; }'
-	CONTENT=$(cat "$SOURCE_FILE" | \
-		# find relevant section (ignore all lines before)
-		sed -n "/$DISTRO_ENTRY/,\$p" | \
-		# find content between "```"
-		sed -n '/```/{:loop n; /```/q; p; b loop}')
+	CONTENT=$(content "$SOURCE_FILE" "$DISTRO_ENTRY")
 	echo -e "$PREFIX\n$CONTENT\n" >"$TARGET_FILE"
+}
+
+writeFiles() {
+	SOURCE_FILE="$1"
+	TARGET="$2"
+	SUBDIR="$3"
+	OVERWRITE="$4"
+	DISTRO_ENTRY="$5"
+	PATTERN="$6"
+
+	PREREQUISITES='50-freetz-ng-prerequisites.sh'
+	DEPSJSON="dependencies.json"
+	writeDepsJsonFile "$SOURCE_FILE" "$TARGET/$SUBDIR/$DEPSJSON" "$OVERWRITE" "$DISTRO_ENTRY" "$PATTERN"
+	writePackageFile  "$SOURCE_FILE" "$TARGET/$SUBDIR/$PREREQUISITES" "$OVERWRITE" "$DISTRO_ENTRY"
 }
 
 
@@ -32,21 +78,21 @@ HTTP_STATUS=`curl --etag-save "$CACHE/$FILENAME.etag" --etag-compare "$CACHE/$FI
 OVERWRITE=false
 [ "$HTTP_STATUS" = 200 ] && OVERWRITE=true
 
-# ❯ LC_ALL=C lsb_release -s --id
-# Ubuntu
-# ❯ LC_ALL=C lsb_release -s --release
-# 22.10
+UBUNTU_PATTERN='^sudo apt-get -y install |^sudo apt -y install '
+DEBIAN_PATTERN="$UBUNTU_PATTERN"
+FEDORA_PATTERN='^sudo dnf -y install '
 
-writePackageFile "$CACHE/$FILENAME" "$TARGET/ubuntu:22.04/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Ubuntu 22"
-writePackageFile "$CACHE/$FILENAME" "$TARGET/ubuntu:20.04/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Ubuntu 20"
-writePackageFile "$CACHE/$FILENAME" "$TARGET/ubuntu:18.04/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Ubuntu 18"
-writePackageFile "$CACHE/$FILENAME" "$TARGET/ubuntu:16.04/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Ubuntu 16"
-writePackageFile "$CACHE/$FILENAME" "$TARGET/ubuntu:14.04/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Ubuntu 14"
+writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:22.04" "$OVERWRITE" ' - Ubuntu 22'     "$UBUNTU_PATTERN"
+writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:20.04" "$OVERWRITE" ' - Ubuntu 20'     "$UBUNTU_PATTERN"
+writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:18.04" "$OVERWRITE" ' - Ubuntu 18'     "$UBUNTU_PATTERN"
+writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:16.04" "$OVERWRITE" ' - Ubuntu 16'     "$UBUNTU_PATTERN"
+writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:14.04" "$OVERWRITE" ' - Ubuntu 14'     "$UBUNTU_PATTERN"
 
-writePackageFile "$CACHE/$FILENAME" "$TARGET/debian:11/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Debian 11"
+writeFiles "$CACHE/$FILENAME" "$TARGET" "debian:11"    "$OVERWRITE" ' - Debian 11'     "$DEBIAN_PATTERN"
 
-writePackageFile "$CACHE/$FILENAME" "$TARGET/fedora:36/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Fedora 36"
-writePackageFile "$CACHE/$FILENAME" "$TARGET/fedora:35/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Fedora 35"
-writePackageFile "$CACHE/$FILENAME" "$TARGET/fedora:34/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Fedora 33\/34"
-writePackageFile "$CACHE/$FILENAME" "$TARGET/fedora:33/50-freetz-ng-prerequisites.sh" "$OVERWRITE" " - Fedora 33\/34"
+# TODO add 'sudo dnf -y groupinstall 'Development Tools' 'Development Libraries'' information to JSON
+writeFiles "$CACHE/$FILENAME" "$TARGET" "fedora:36"    "$OVERWRITE" ' - Fedora 36'     "$FEDORA_PATTERN"
+writeFiles "$CACHE/$FILENAME" "$TARGET" "fedora:35"    "$OVERWRITE" ' - Fedora 35'     "$FEDORA_PATTERN"
+writeFiles "$CACHE/$FILENAME" "$TARGET" "fedora:34"    "$OVERWRITE" ' - Fedora 33\/34' "$FEDORA_PATTERN"
+writeFiles "$CACHE/$FILENAME" "$TARGET" "fedora:33"    "$OVERWRITE" ' - Fedora 33\/34' "$FEDORA_PATTERN"
 
