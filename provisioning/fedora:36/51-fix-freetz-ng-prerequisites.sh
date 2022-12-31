cf8fc7bebc7e9ba30c8f92f6f746bdbc90577850
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2022-12-31 19:46:18 +0100

    installing fastjar from rpmsphere

--- provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
@@ -3,3 +3,7 @@
 dnf -y install rpcgen python-unversioned-command javapackages-tools
 update-alternatives --install /usr/bin/javac javac /usr/bin/ecj 20
 
+# program     fastjar                FREETZ_PACKAGE_CLASSPATH
+FILE=fastjar-0.98-5.1.x86_64.rpm
+wget -c "https://raw.githubusercontent.com/rpmsphere/x86_64/master/f/$FILE" && dnf -y install "$FILE" && rm "$FILE"
+
--- provisioning/fedora:37/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/fedora:37/51-fix-freetz-ng-prerequisites.sh
@@ -1,5 +1,9 @@
 # program     rpcgen                 FREETZ_PACKAGE_NFS_UTILS FREETZ_PACKAGE_AUTOFS
 # program     javac                  FREETZ_PACKAGE_CLASSPATH
-dnf -y install readline-devel rpcgen python-unversioned-command javapackages-tools
+dnf -y install rpcgen python-unversioned-command javapackages-tools readline-devel
 update-alternatives --install /usr/bin/javac javac /usr/bin/ecj 20
+
+# program     fastjar                FREETZ_PACKAGE_CLASSPATH
+FILE=fastjar-0.98-5.1.x86_64.rpm
+wget -c "https://raw.githubusercontent.com/rpmsphere/x86_64/master/f/$FILE" && dnf -y install "$FILE" && rm "$FILE"
 
