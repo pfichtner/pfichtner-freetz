aba333f91f9f58b0b746e8fbecf0b5ab13bd2da4
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2023-01-11 20:42:18 +0100

    break the build if presumed absent packages are alreay present

--- provisioning/debian:11/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/debian:11/51-fix-freetz-ng-prerequisites.sh
@@ -1,5 +1,17 @@
 # .prerequisites: program     xml2-config            FREETZ_PACKAGE_GNTPSEND FREETZ_PACKAGE_ASTERISK
 # program     javac                  FREETZ_PACKAGE_CLASSPATH
 # ecj depends on java-wrappers but ths dependency is missing in ecj, https://salsa.debian.org/java-team/ecj/-/commit/f7d6cae26fbde9b146d02affe08da534f801a0f7
+
+error() {
+	echo "$1"
+	exit 1
+}
+
+expectToBeAbsent() {
+	dpkg -l "$1" && error "$1 was expected to be absent but was present"
+}
+
+expectToBeAbsent libxml2-dev
+expectToBeAbsent java-wrappers
 apt-get -y install libxml2-dev java-wrappers
 
--- provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/fedora:36/51-fix-freetz-ng-prerequisites.sh
@@ -1,5 +1,18 @@
 # program     rpcgen                 FREETZ_PACKAGE_NFS_UTILS FREETZ_PACKAGE_AUTOFS
 # program     javac                  FREETZ_PACKAGE_CLASSPATH
+
+error() {
+	echo "$1"
+	exit 1
+}
+
+expectToBeAbsent() {
+	dnf list installed "$1" >/dev/null && error "$1 was expected to be absent but was present"
+}
+
+expectToBeAbsent rpcgen 
+expectToBeAbsent python-unversioned-command
+expectToBeAbsent javapackages-tools
 dnf -y install rpcgen python-unversioned-command javapackages-tools
 update-alternatives --install /usr/bin/javac javac /usr/bin/ecj 20
 
