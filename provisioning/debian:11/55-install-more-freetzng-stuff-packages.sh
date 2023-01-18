80b02a4c4911e4978b58d6929744cd5692083ca2
Peter Fichtner (pfichtner) <github.07.fichtner@0sg.net>
2023-01-18 23:02:58 +0100

    comment added

--- provisioning/debian:11/55-install-more-freetzng-stuff-packages.sh
+++ provisioning/debian:11/55-install-more-freetzng-stuff-packages.sh
@@ -2,6 +2,9 @@
 apt-get -y install patchutils
 
 # not necessary for building but uploading via tools/push_firmware
+# they USED to be not part of the PREREQUISITES but now they are. 
+# Neverless we enforce the installation here anyway
+
 # iproute2: /bin/ip /sbin/bridge /sbin/ip (not needed but useful)
 # ncftp: /usr/bin/ncftpput
 # iputils-ping: /bin/ping
