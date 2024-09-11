205304a605b8c88b4880269d1b63ba309b35d2ca
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-09-11 08:48:28 +0200

    removed ubuntu:23.xx since EOL

--- scripts/generate-prerequisites.sh
+++ scripts/generate-prerequisites.sh
@@ -110,8 +110,6 @@ DEBIAN_PATTERN="$UBUNTU_PATTERN"
 FEDORA_PATTERN='^sudo dnf -y install '
 
 writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:24.04" "$OVERWRITE" ' - Ubuntu 23\/24'     "$UBUNTU_PATTERN"
-writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:23.10" "$OVERWRITE" ' - Ubuntu 23\/24'     "$UBUNTU_PATTERN"
-writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:23.04" "$OVERWRITE" ' - Ubuntu 23\/24'     "$UBUNTU_PATTERN"
 writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:22.04" "$OVERWRITE" ' - Ubuntu 22'         "$UBUNTU_PATTERN"
 writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:20.04" "$OVERWRITE" ' - Ubuntu 20'         "$UBUNTU_PATTERN"
 writeFiles "$CACHE/$FILENAME" "$TARGET" "ubuntu:18.04" "$OVERWRITE" ' - Ubuntu 18'         "$UBUNTU_PATTERN"
