5b43c261048599d677e1c38fa163d86a30e303b1
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2025-11-21 22:56:52 +0100

    u26.04: purge rust coreutils

---
+++ provisioning/ubuntu:26.04/90-purge-rust-coreutils.sh
@@ -0,0 +1,3 @@
+# pseudo not working with rust-coreutils https://www.reddit.com/r/linux/comments/1nr1bas/ubuntu_2510s_move_to_rust_coreutils_is_causing/
+apt remove --allow-remove-essential coreutils-from-uutils
+
