e7fac3c4c11eae7a92e47338e8d2dd55864664f5
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2026-04-01 10:03:09 +0000

    fix for install gosu on fedora
    Co-Authored-By: ThomasToka <117008525+thomastoka@users.noreply.github.com>

--- provisioning/fedora:43/55-install-dockerimage-packages.sh
+++ provisioning/fedora:43/55-install-dockerimage-packages.sh
@@ -1,9 +1,27 @@
-# add repo
-dnf -y install dnf-utils
-. /etc/os-release 
-rpm --import "http://downloadcontent.opensuse.org/repositories/home:/alvistack/Fedora_$VERSION_ID/repodata/repomd.xml.key"
-yum-config-manager --add-repo "http://downloadcontent.opensuse.org/repositories/home:/alvistack/Fedora_$VERSION_ID"
+set -eux
 
-# our docker entrypoint relies on gosu to switch to unprivileged user
-dnf -y install gosu
+dnf -y install gnupg
+
+GOSU_VERSION=1.19
+ARCH="$(uname -m)"
+
+case "$ARCH" in
+	x86_64) GOSU_ARCH=amd64 ;;
+	aarch64) GOSU_ARCH=arm64 ;;
+	armv7l) GOSU_ARCH=armhf ;;
+	*) echo "Unsupported architecture: $ARCH" && exit 1 ;;
+esac
+
+curl -fsSL -o /usr/local/bin/gosu \
+	"https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${GOSU_ARCH}"
+
+curl -fsSL -o /usr/local/bin/gosu.asc \
+	"https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${GOSU_ARCH}.asc"
+
+# Verify signature
+export GNUPGHOME="$(mktemp -d)"
+gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys B42F6819007F00F88E364FD4036A9C25BF357DD4
+gpg --batch --verify /usr/local/bin/gosu.asc /usr/local/bin/gosu
+
+chmod +x /usr/local/bin/gosu
 
