a4618ead1db2a000a18109ce27c9e4829c767534
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2026-04-01 10:03:09 +0000

    fix for install gosu on fedora
    Co-Authored-By: ThomasToka <117008525+thomastoka@users.noreply.github.com>

--- provisioning/fedora:41/55-install-dockerimage-packages.sh
+++ provisioning/fedora:41/55-install-dockerimage-packages.sh
@@ -6,17 +6,17 @@ GOSU_VERSION=1.19
 ARCH="$(uname -m)"
 
 case "$ARCH" in
-  x86_64) GOSU_ARCH=amd64 ;;
-  aarch64) GOSU_ARCH=arm64 ;;
-  armv7l) GOSU_ARCH=armhf ;;
-  *) echo "Unsupported architecture: $ARCH" && exit 1 ;;
+	x86_64) GOSU_ARCH=amd64 ;;
+	aarch64) GOSU_ARCH=arm64 ;;
+	armv7l) GOSU_ARCH=armhf ;;
+	*) echo "Unsupported architecture: $ARCH" && exit 1 ;;
 esac
 
 curl -fsSL -o /usr/local/bin/gosu \
-  "https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${GOSU_ARCH}"
+	"https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${GOSU_ARCH}"
 
 curl -fsSL -o /usr/local/bin/gosu.asc \
-  "https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${GOSU_ARCH}.asc"
+	"https://github.com/tianon/gosu/releases/download/${GOSU_VERSION}/gosu-${GOSU_ARCH}.asc"
 
 # Verify signature
 export GNUPGHOME="$(mktemp -d)"
