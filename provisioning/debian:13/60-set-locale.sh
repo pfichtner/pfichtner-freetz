bc08beba6f37cf4a013e94048a5badc4b06bbbe3
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2026-08-16 15:36:26 +0000

    fix: redirect error output for locale generation failure

--- provisioning/debian:13/60-set-locale.sh
+++ provisioning/debian:13/60-set-locale.sh
@@ -1,3 +1,3 @@
 command -v locale-gen >/dev/null 2>&1 || apt-get -y install locales
 sed -i 's/^# *en_US\.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen
-locale -a | grep -Ei '^(en_US)' || (echo "failed to generate locales" && exit 1)
+locale -a | grep -Ei '^(en_US)' || (echo "failed to generate locales" >&2 && exit 1)
