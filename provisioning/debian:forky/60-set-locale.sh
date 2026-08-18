b82e62edd2debcf4f6037d3c3fbc8c131bcaf745
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2026-08-18 11:21:20 +0000

    fix: update locale generation to ensure en_US.UTF-8 is properly set (again)

--- provisioning/debian:forky/60-set-locale.sh
+++ provisioning/debian:forky/60-set-locale.sh
@@ -1,3 +1,3 @@
 command -v locale-gen >/dev/null 2>&1 || apt-get -y install locales
-locale-gen en_US.UTF-8
-
+sed -i 's/^# *en_US\.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen
+locale -a | grep -qEi '^(en_US)' || (echo "failed to generate locales" >&2 && exit 1)
