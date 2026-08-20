ac0fcdd6135e67f12207dd320bd05a08e383e4ce
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2026-08-20 17:44:09 +0200

    fixing arch issue of libatomic, fixes https://github.com/Freetz-NG/freetz-ng/pull/1574

--- provisioning/fedora:44/51-fix-freetz-ng-prerequisites.sh
+++ provisioning/fedora:44/51-fix-freetz-ng-prerequisites.sh
@@ -11,3 +11,8 @@ expectToBeAbsent() {
 	dnf list installed "$1" >/dev/null 2>&1 && error "$1 was expected to be absent but was present"
 }
 
+# gcc.x86_64 requires "libatomic" without architecture qualifier.
+# DNF may resolve this to the i686 variant, which installs to /usr/lib/ instead of /usr/lib64/.
+# Explicitly install the x86_64 variant so the linker finds libatomic.so.1.2.0.
+dnf install -y libatomic.x86_64
+
